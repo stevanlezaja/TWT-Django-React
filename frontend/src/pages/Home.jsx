0384b14ad6d7294a11e369f0bcbc230d4ae71d46
Stevan Lezaja <stevan.lezaja@gmail.com>
2024-05-05 16:54:18 +0200

    create, read, delete notes in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,46 @@
+import {useState, useEffect} from 'react';
+import api from '../api';
+
 function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState('');
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+    
+    const getNotes = () => {
+        api
+        .get('/api/notes/')
+        .then((res) => res.data)
+        .then((data) => {setNotes(data); console.log(data) })
+        .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert('Note deleted successfully');
+                else alert('Something went wrong');
+            })
+            .catch((error) => alert(error));
+        getNotes();
+    }
+
+    const createNote = (e) => {
+        e.preventDefault();
+        api
+            .post('/api/notes', { content, title })
+            .then((res) => {
+                if (res.status === 201) alert('Note created successfully');
+                else alert('Something went wrong');
+            })
+            .catch((error) => alert(error));
+        getNotes();
+    }
+
     return <div>Home</div>
 }
 
