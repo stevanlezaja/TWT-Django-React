938cf5eec1e016cfa667079b4c9d1d29d69958ed
Stevan Lezaja <stevan.lezaja@gmail.com>
2024-05-05 15:54:30 +0200

    form bug fix

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import api from '../api'
 import {useNavigate} from 'react-router-dom'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
+import '../styles/Form.css'
 
 function Form({route, method}){
     const [username, setUsername] = useState('')
@@ -11,11 +12,11 @@ function Form({route, method}){
 
     const name = method === "login" ? "Login" : "Register"
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault()
-
-        try{
+ 
+        try {
             const res = await api.post(route, {username, password})
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
@@ -34,17 +35,17 @@ function Form({route, method}){
     return <form onSubmit={handleSubmit} className="form-container">
         <h1>{name}</h1>
         <input
-            className='form-input'
-            type='text'
+            className="form-input"
+            type="text"
             value={username}
-            onCanPlay={(e) => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             placeholder='Username'
         />
         <input
             className='form-input'
             type='password'
             value={password}
-            onCanPlay={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder='Password'
         />
         <button className='form-button' type='submit'>
@@ -52,3 +53,5 @@ function Form({route, method}){
         </button>
     </form>
 }
+
+export default Form
